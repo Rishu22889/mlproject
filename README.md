72add8e09c575bb7770587247cdc2042e6ad31d8
Rishu <rk1258106@gmail.com>
2026-04-03 01:22:10 +0530

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # End to End Data Science Project
+
+import dagshub
+dagshub.init(repo_owner='Rishu22889', repo_name='mlproject', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
+
+MLFLOW_TRACKING_URI = https://dagshub.com/Rishu22889/mlproject.mlflow
--- src/ml_project/components/model_trainer.py
+++ src/ml_project/components/model_trainer.py
@@ -2,8 +2,10 @@ import os
 import sys
 
 from dataclasses import dataclass
+from urllib.parse import urlparse
 
-from sklearn.metrics import f1_score
+import mlflow
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -16,6 +18,10 @@ from src.ml_project.logger import logging
 from src.ml_project.exception import CustomException
 from src.ml_project.utils import save_object, evaluate_models
 
+import dagshub
+
+dagshub.init(repo_owner='Rishu22889', repo_name='mlproject', mlflow=True)
+
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path: str = os.path.join("artifacts", "model.pkl")
@@ -23,6 +29,13 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
+
+    def eval_metrics(self, y_true, y_pred):
+        accuracy = accuracy_score(y_true, y_pred)
+        precision = precision_score(y_true, y_pred)
+        recall = recall_score(y_true, y_pred)
+        f1 = f1_score(y_true, y_pred)
+        return accuracy, precision, recall, f1
 
     def initiate_model_trainer(self, train_array, test_array):
         try:
@@ -94,6 +107,39 @@ class ModelTrainer:
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
             best_model = models[best_model_name]
+
+            print("Best Model Found: ", best_model_name)
+
+            model_names = list(params.keys())
+
+            actual_model = ""
+
+            for model in model_names:
+                if best_model_name == model:
+                    actual_model += model
+            
+            best_params = params[actual_model]
+
+            # MlFlow
+            mlflow.set_registry_uri("https://dagshub.com/Rishu22889/mlproject.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            with mlflow.start_run(run_name="Model Training"):
+                predicted_qualities = best_model.predict(X_test)
+
+                (accuracy, precision, recall, f1) = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_param("best_params", best_params)
+                mlflow.log_metric("accuracy", accuracy)
+                mlflow.log_metric("precision", precision)
+                mlflow.log_metric("recall", recall)
+                mlflow.log_metric("f1_score", f1)
+
+                if tracking_url_type_store != "file":
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
+
             if best_model_score<0.6:
                 raise CustomException("No suitable model found")
             logging.info(f"Best model found on both training and testing dataset is {best_model_name} with F1 score: {best_model_score}")
