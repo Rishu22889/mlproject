2c75dccfdeeda2d9977d51324d4f46f254a6fe6d
Rishu <rk1258106@gmail.com>
2026-04-02 12:19:36 +0530

    data ingestion

---
+++ app.py
@@ -0,0 +1,16 @@
+from src.ml_project.logger import logging
+from src.ml_project.exception import CustomException
+from src.ml_project.components.data_ingestion import DataIngestion, DataIngestionConfig
+import sys
+
+if __name__=="__main__":
+    logging.info("The execution has started")
+
+    try:
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+        
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e, sys)
+    
